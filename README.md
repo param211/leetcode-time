6c93ec3058f21fd940480f2b2aa6a4f7cea5b7b6
param211 <paramchauhan21@gmail.com>
2021-08-19 17:28:08 +0530

    Onboard to git

---
+++ README.md
@@ -0,0 +1,10 @@
+# Show current time on leetcode
+Helps keep track of time when in full screen mode or in an os that doesn't always show current time.
+
+![Demo](https://i.imgur.com/Lkod51v.png)
+### Steps
+* Clone this repository
+
+* Open chrome and go to `chrome://extensions/`
+
+* Click on `Load unpacked` and select the repository path
---
+++ time.js
@@ -0,0 +1,20 @@
+console.log("Leetcode Timer started")
+
+window.onload = function() {
+    setTimeout(showTime, 3000);
+    
+    function showTime() {
+      console.log("showTime called")
+      const today = new Date();
+      let h = today.getHours();
+      let m = today.getMinutes();
+      m = checkTime(m);
+      document.getElementsByClassName('css-2efsh5-StyledSpan')[0].innerHTML =  h + ":" + m;
+      setTimeout(showTime, 60000);
+    }
+
+    function checkTime(i) {
+      if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+      return i;
+    }
+};
