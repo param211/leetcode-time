da2c4a72630f890faaeb20bf4a52e778b1863a54
param211 <paramchauhan21@gmail.com>
2021-08-20 17:09:40 +0530

    Add persistence

--- time.js
+++ time.js
@@ -1,4 +1,3 @@
-window.onload = function() {
     setTimeout(showTime, 3000);
     
     function showTime() {
@@ -6,7 +5,10 @@ window.onload = function() {
       let h = today.getHours();
       let m = today.getMinutes();
       m = checkTime(m);
-      document.getElementsByClassName('css-2efsh5-StyledSpan')[0].innerHTML =  h + ":" + m;
+      var x = document.getElementsByClassName('css-2efsh5-StyledSpan')[0] 
+      if(x != null) {
+        x.innerHTML =  h + ":" + m;
+      }
       setTimeout(showTime, 60000);
     }
 
@@ -14,4 +16,5 @@ window.onload = function() {
       if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
       return i;
     }
-};
+
+// 60000 -> 60 sec
